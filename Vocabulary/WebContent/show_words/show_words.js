58a7ed9190c5453fc93fb1562f60e7835d0407d3
Doron <Doron@Lenovo-PC>
2016-01-04 09:24:45 +0200

    add random list

--- Vocabulary/WebContent/show_words/show_words.js
+++ Vocabulary/WebContent/show_words/show_words.js
@@ -1,9 +1,11 @@
- 
+ var showWordsApp = angular.module("showWordsApp", []);
+ showWordsApp.controller('showWords', function () {
 
+     this.wordsToLearn = JSON.parse(text);
+     this.random = function () {
+         return 0.5 - Math.random();
 
-var showWordsApp = angular.module("showWordsApp", []);
-showWordsApp.controller('showWords', function () {
- 
-  this.wordsToLearn = JSON.parse(text);
+     };
+    
 
-});
+ });
--- Vocabulary/WebContent/show_words/word_table.js
+++ Vocabulary/WebContent/show_words/word_table.js
@@ -1,4 +1,11 @@
    var text = '{ "words" : [' +
-'{ "eng":"Dog" , "heb":"כלב" },' +
-'{ "eng":"Cat" , "heb":"חתול" },' +
-'{ "eng":"Frog" , "heb":"צפרדע" } ]}';
+       '{ "eng":"Dog" , "heb":"כלב" },' +
+       '{ "eng":"Cat" , "heb":"חתול" },' +
+       '{ "eng":"Elephant" , "heb":"פיל" },' +
+       '{ "eng":"Fish" , "heb":"דג" },' +
+       '{ "eng":"Spider" , "heb":"עכביש" },' +
+       '{ "eng":"Zebra" , "heb":"זברה" },' +
+       '{ "eng":"Shark" , "heb":"כריש" },' +
+       '{ "eng":"Tree" , "heb":"עץ" },' +
+       '{ "eng":"Flower" , "heb":"פרח" },' +
+       '{ "eng":"Frog" , "heb":"צפרדע" }]}'
