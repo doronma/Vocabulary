357095863fb37f0e05aaa51e1cfff08128e3e0ee
Doron Marcus <marcus.doron@gmail.com>
2016-01-05 16:48:47 +0200

    add random function

--- Vocabulary/WebContent/show_words/show_words.js
+++ Vocabulary/WebContent/show_words/show_words.js
@@ -1,5 +1,23 @@
 var showWordsApp = angular.module("showWordsApp", []);
 showWordsApp.controller('showWords', function() {
+	
+	this.shuffle = function(array) {
+		  var currentIndex = array.length, temporaryValue, randomIndex;
+
+		  // While there remain elements to shuffle...
+		  while (0 !== currentIndex) {
+
+		    // Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+
+		    // And swap it with the current element.
+		    temporaryValue = array[currentIndex];
+		    array[currentIndex] = array[randomIndex];
+		    array[randomIndex] = temporaryValue;
+		  }
+		  return array;
+		}
 
 	this.wordsToLearn = JSON.parse(text);
 	this.random = function() {
@@ -8,21 +26,28 @@ showWordsApp.controller('showWords', function() {
 	};
 	this.words = this.wordsToLearn.words;
 	this.wordList = this.words.slice(0);
+	this.shuffle(this.wordList);
 	
 	this.counter = 0;
 	this.count = function() {
-		if (this.counter < this.words.length) {
+		if (this.counter < this.words.length-1) {
 			this.counter++;
 		} else {
 			alert('no more');
 		}
 	}
 	this.selectWord = function(word) {
-		if (word == this.words[this.counter].eng) {
+		if (word == this.wordList[this.counter].eng) {
 			this.count();
 		}else{
 			alert('bad');
 		}
 	}
+	this.reset = function(){
+		this.counter = 0;
+		this.shuffle(this.wordList);
+	}
+	
+	
 
 });
