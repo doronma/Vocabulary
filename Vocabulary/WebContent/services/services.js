3a1ec0785f04035be42b61d4dbbaeeb7434c3e5b
Doron Marcus <marcus.doron@gmail.com>
2016-01-18 18:56:06 +0200

    set ability to get every thing from the server

--- Vocabulary/WebContent/services/services.js
+++ Vocabulary/WebContent/services/services.js
@@ -39,48 +39,27 @@ vocServices.service('wordManagerServer', function($http) {
 			return result.data
 		})
 	}
+	this.getWordGroup = function(groupName){
+		var currentURL = 'http://localhost:8080/getWordsExByName?groupName=' + groupName;
+		return $http({
+			method : "GET",
+			url : currentURL
+		}).then(function(result) {
+			return result.data
+		})
+	}
 
 });
 
 vocServices.service('wordManager', function(wordManagerServer) {
-
-	this.animals = '{ "vocabularyWordList" : ['
-			+ '{ "engWord":"Dog" , "hebWord":"כלב" },'
-			+ '{ "engWord":"Cat" , "hebWord":"חתול" },'
-			+ '{ "engWord":"Elephant" , "hebWord":"פיל" },'
-			+ '{ "engWord":"Fish" , "hebWord":"דג" },'
-			+ '{ "engWord":"Spider" , "hebWord":"עכביש" },'
-			+ '{ "engWord":"Zebra" , "hebWord":"זברה" },'
-			+ '{ "engWord":"Shark" , "hebWord":"כריש" },'
-			+ '{ "engWord":"Tree" , "hebWord":"עץ" },'
-			+ '{ "engWord":"Flower" , "hebWord":"פרח" },'
-			+ '{ "engWord":"Frog" , "hebWord":"צפרדע" }]}';
-
-	this.weekDays1 = '{ "vocabularyWordList" : ['
-			+ '{ "engWord":"Sunday1" , "hebWord":"יום ראשון" },'
-			+ '{ "engWord":"Monday" , "hebWord":"יום שני" },'
-			+ '{ "engWord":"Tuesday" , "hebWord":"יום שלישי" },'
-			+ '{ "engWord":"Wednesday" , "hebWord":"יום רביעי" },'
-			+ '{ "engWord":"Thursday" , "hebWord":"יום חמישי" },'
-			+ '{ "engWord":"Friday" , "hebWord":"יום שישי" },'
-			+ '{ "engWord":"Saturday" , "hebWord":"יום שבת" }]}';
-
 	
 	this.getCurrentWordList = function() {
-		return this.wordsToLearn.vocabularyWordList;
+		return this.currentWordList.wordList;
 	}
 
 	
-
-	this.updateWordList = function(selectedGroupName) {
-		if (selectedGroupName == "Animals") {
-			this.wordsToLearn = JSON.parse(this.animals);
-		}
-		if (selectedGroupName == "WeekDays") {
-			this.wordsToLearn = this.weekDays;
-		}
-		this.setSelectedGroupName(selectedGroupName);
-
+	this.setCurrentWordList = function(newWordList){
+		this.currentWordList = newWordList;
 	}
 
 	this.getSelectedGroupName = function() {
@@ -90,23 +69,5 @@ vocServices.service('wordManager', function(wordManagerServer) {
 	this.setSelectedGroupName = function(selectedGroupName){
 		this.selectedGroupName = selectedGroupName;
 	}
-
-	this.init = function() {
-		//this.wordGroup = JSON.parse(this.wordGroupJson);
-		//this.updateWordList('WeekDays');
-		var wordManagerService = this;
-
-		wordManagerServer.getData().then(function(result) {
-			wordManagerService.weekDays = result;
-			
-			
-			wordManagerService.updateWordList('WeekDays');
-		});
-		
-		
-
-
-	}
-
-	this.init();
+	
 });
--- Vocabulary/WebContent/wordGroupSelection/wordGroupSelection.js
+++ Vocabulary/WebContent/wordGroupSelection/wordGroupSelection.js
@@ -4,15 +4,28 @@ wordGroupSelectionApp.controller('wordGroupSelection', function(wordManager,
 		wordManagerServer) {
 
 	this.selectRow = function(groupName) {
-		wordManager.updateWordList(groupName);
-		this.selectedGroupName = wordManager.getSelectedGroupName();
+		wordManager.setSelectedGroupName(groupName);
+		this.selectedGroupName = groupName
+		wordManagerServer.getWordGroup(groupName).then(function(result) {
+			wordManager.setCurrentWordList(result)
+		});
 	}
-	this.init = function() {
+
+	this.getWordGroupNameList = function() {
 		var currentController = this;
-		wordManagerServer.getWordGroupNames().then(function(result) {
-			currentController.wordGroupNameList = result;
-			currentController.selectedGroupName = currentController.wordGroupNameList[0]
-		})
+		wordManagerServer.getWordGroupNames().then(
+				function(result) {
+					currentController.wordGroupNameList = result;
+					// set first group as current
+					// grroup
+					currentController
+							.selectRow(currentController.wordGroupNameList[0]);
+				})
+	}
+
+	// fetch data on startup
+	this.init = function() {
+		this.getWordGroupNameList();
 	}
 	this.init();
 
